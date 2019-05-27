b9530dabd8311c51870ead0c784a7e0ee2db5dbc
Jahir Santamaria <jahiralex@gmail.com>
2019-05-26 22:23:00 -0500

    Configuración inicial del proyecto

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,22 +9,52 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
-  constructor( private http: HttpClient ) { 
+  constructor( private http: HttpClient ) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    //Leer archivo JSON
-    this.http.get('https://entresabanas-f684c.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto[]) => {
-    console.log(resp);
-    this.productos = resp;
-    this.cargando = false;
-
-    setTimeout(() =>  {
+    return new Promise( ( resolve, reject ) => {
+      this.http.get('https://entresabanas-f684c.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[]) => {
+      this.productos = resp;
       this.cargando = false;
-    }, 2000);
+      resolve();
+      });
     });
+ }
+
+ getProducto( id: string) {
+   return this.http.get(`https://entresabanas-f684c.firebaseio.com/productos/${ id }.json`);
+ }
+
+ buscarProducto( termino: string ) {
+   if( this.productos.length === 0 ) {
+     this.cargarProductos().then ( () => {
+        this.filtrarProductos( termino );
+     });
+   } else {
+    this.filtrarProductos( termino );
+   }
+
+   /*this.productosFiltrado =  this.productos.filter( producto => {
+     return true;
+   });
+   console.log( this.productosFiltrado);*/
+ }
+
+ private filtrarProductos( termino: string ) {
+   console.log(this.productos);
+   this.productosFiltrado = [];
+   termino = termino.toLocaleLowerCase();
+
+   this.productos.forEach (prod => {
+     const tituloLower = prod.titulo.toLocaleLowerCase();
+     if ( prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf( termino ) >= 0  ){
+       this.productosFiltrado.push(prod);
+     }
+   });
  }
 }
